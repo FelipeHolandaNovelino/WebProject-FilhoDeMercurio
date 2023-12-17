d76f16120f05287615a13f79f5275ad39f9e933e
FelipeNovelino <holanda.felipe@hotmail.com>
2023-12-17 19:26:31 -0300

    Projeto Web - Filho de Mercúrio1.0

---
+++ README.md
@@ -0,0 +1,14 @@
+# README.MD
+
+---
+
+# **Projeto Web - Filho de Mercúrio**
+https://filhodemercurio.netlify.app/ você pode acessar por esse link. 
+
+---
+
+A página web 'Filho de Mercúrio' foi criada como um e-commerce dedicado à comercialização de quadros e tecidos estampados. A página oferece a opção de navegar entre os diferentes tipos de produtos disponíveis.
+
+Além disso, há um carrinho de compras que permite aos usuários visualizar todos os itens que foram adicionados a ele, juntamente com o cálculo do valor total dos produtos. No carrinho, também é possível remover os produtos que foram adicionados.
+
+A funcionalidade de adicionar um novo quadro ou tecido à lista de produtos está disponível. Assim que um item é adicionado, ele é exibido imediatamente na tela.
---
+++ scripts.js
@@ -0,0 +1,444 @@
+const produtos = [
+  {
+    nome: "Janelas 2.0",
+    descricao: "Descrição da obra de arte 1.",
+    preco: 50.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/164/original/produto1.PNG?1699150345",
+    tipo: "Quadros",
+  },
+  {
+    nome: "Obra de Arte 2",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/496/original/produto2.PNG?1699214150",
+    tipo: "Quadros",
+  },
+  {
+    nome: "Obra de Arte 3",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/497/original/produto3.PNG?1699214216",
+    tipo: "Quadros",
+  },
+  {
+    nome: "Obra de Arte 4",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/677/original/produto4.PNG?1699250687",
+    tipo: "Quadros",
+  },
+  {
+    nome: "Obra de Arte 5",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/679/full/produto5.PNG?1699250802",
+    tipo: "Quadros",
+  },
+  {
+    nome: "Obra de Arte 6",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/680/original/produto6.PNG?1699250828",
+    tipo: "Quadros",
+  },
+  {
+    nome: "Obra de Arte 7",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/681/original/produto7.PNG?1699250880",
+    tipo: "Quadros",
+  },
+  {
+    nome: "Obra de Arte 8",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/682/original/produto8.PNG?1699250913",
+    tipo: "Quadros",
+  },
+  {
+    nome: "Obra de Arte 9",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/683/original/produto9.PNG?1699250999",
+    tipo: "Quadros",
+  },
+  {
+    nome: "Obra de Arte 10",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/684/original/produto10.PNG?1699251059",
+    tipo: "Quadros",
+  },
+  {
+    nome: "Obra de Arte 11",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/685/original/produto11.PNG?1699251103",
+    tipo: "Quadros",
+  },
+  {
+    nome: "Obra de Arte 12",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/693/original/produto19.PNG?1699251340",
+
+    tipo: "Quadros",
+  },
+  {
+    nome: "Obra de Arte 13",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/687/original/produto13.PNG?1699251149",
+    tipo: "Tecidos",
+  },
+  {
+    nome: "Obra de Arte 14",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/688/original/produto14.PNG?1699251174",
+    tipo: "Tecidos",
+  },
+  {
+    nome: "Obra de Arte 15",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/689/original/produto15.PNG?1699251196",
+    tipo: "Tecidos",
+  },
+  {
+    nome: "Obra de Arte 16",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/690/original/produto16.PNG?1699251218",
+    tipo: "Tecidos",
+  },
+  {
+    nome: "Obra de Arte 17",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/691/original/produto17.PNG?1699251285",
+    tipo: "Tecidos",
+  },
+  {
+    nome: "Obra de Arte 18",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/692/original/produto18.PNG?1699251315",
+    tipo: "Tecidos",
+  },
+  {
+    nome: "Obra de Arte 19",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/686/original/produto12.PNG?1699251125",
+    tipo: "Tecidos",
+  },
+  {
+    nome: "Obra de Arte 20",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/694/original/produto20.PNG?1699251382",
+    tipo: "Tecidos",
+  },
+  {
+    nome: "Obra de Arte 21",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/695/original/produto21.PNG?1699251417",
+    tipo: "Tecidos",
+  },
+  {
+    nome: "Obra de Arte 22",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/696/original/produto22.PNG?1699251439",
+    tipo: "Tecidos",
+  },
+  {
+    nome: "Obra de Arte 23",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem:
+      "https://uploaddeimagens.com.br/images/004/656/698/original/produto23.PNG?1699251461",
+    tipo: "Tecidos",
+  },
+  {
+    nome: "Obra de arte 24",
+    descricao: "Descrição da obra de arte 2.",
+    preco: 75.0,
+    imagem: "https://uploaddeimagens.com.br/images/004/656/699/original/produto24.PNG?1699251480",
+  },
+
+];
+
+// Selecione as duas divs de produtos-container para "Quadros" e "Tecidos"
+const quadrosContainer = document.getElementById("quadros-container");
+const tecidosContainer = document.getElementById("tecidos-container");
+
+//cria elementos nas divs de quadros e tecidos
+function atualizarProdutosNaPagina() {
+  quadrosContainer.innerHTML = ""; // Limpa a divisão "Quadros"
+  tecidosContainer.innerHTML = ""; // Limpa a divisão "Tecidos"
+  produtos.forEach((produto) => {
+
+    const produtoDiv = document.createElement("div");
+    produtoDiv.classList.add("produto");
+
+    const imagem = document.createElement("img");
+    imagem.src = produto.imagem;
+    imagem.alt = produto.nome;
+
+    const linhaDivisoria = document.createElement("div");
+    linhaDivisoria.classList.add("linha-divisoria");
+
+    const titulo = document.createElement("h3");
+    titulo.textContent = produto.nome;
+
+    const descricao = document.createElement("p");
+    descricao.textContent = produto.descricao;
+
+    const preco = document.createElement("span");
+    preco.textContent = ` ${produto.preco.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    })}`;
+
+    const comprarButton = document.createElement("button");
+    comprarButton.textContent = "Comprar"; // Texto do botão
+
+    // Adicione os elementos à div de produtos
+    produtoDiv.appendChild(imagem);
+    produtoDiv.appendChild(linhaDivisoria); // Adicione a linha divisória
+    produtoDiv.appendChild(titulo);
+    produtoDiv.appendChild(descricao);
+    produtoDiv.appendChild(preco);
+    produtoDiv.appendChild(comprarButton); // Adicione o botão "Comprar"
+
+    // Adicione a div de produto à div de produtos apropriada com base no tipo de produto
+    if (produto.tipo === "Quadros") {
+      quadrosContainer.appendChild(produtoDiv);
+    } else if (produto.tipo === "Tecidos") {
+      tecidosContainer.appendChild(produtoDiv);
+    }
+  });
+}
+
+// Chame a função para atualizar os produtos na página
+atualizarProdutosNaPagina();
+
+
+
+
+
+/***************************************************** */
+
+
+//carrinho
+
+// Um array vazio para representar o carrinho de compras
+const carrinho = [];
+
+// Atualize o número de itens no carrinho
+const carrinhoQuantidade = document.querySelector(".carrinho-quantidade");
+
+//Atualizar contador quando o array carrinho for modificado
+function atualizarContadorItensCarrinho() {
+  carrinhoQuantidade.textContent = carrinho.length;
+}
+
+// Adicione um ouvinte de evento aos botões "Comprar"
+const comprarButtons = document.querySelectorAll(".produto button");
+comprarButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    const produto = produtos[index];
+    carrinho.push(produto); // Adicione o produto ao carrinho
+    atualizarContadorItensCarrinho();
+    renderizarItensCarrinho();
+    console.log(carrinho);
+  });
+});
+
+
+
+/*********************** */
+
+
+
+//barra carrinho
+
+// Seleciona o botão de carrinho
+const carrinhoButton = document.getElementById("carrinho-button");
+
+// Seleciona o botão "Fechar Carrinho"
+const fecharCarrinhoButton = document.getElementById("fechar-carrinho");
+
+// Seleciona a barra de rolagem do carrinho
+const carrinhoBarraRolagem = document.getElementById("carrinho-barra-rolagem");
+
+//Evento para o botão de carrinho
+carrinhoButton.addEventListener("click", () => {
+  carrinhoBarraRolagem.style.display = "block"; // Abre a barra de rolagem
+});
+
+//Evento para o botão "Fechar Carrinho"
+fecharCarrinhoButton.addEventListener("click", () => {
+  carrinhoBarraRolagem.style.display = "none"; // Fecha a barra de rolagem
+});
+
+
+
+
+
+//************************************/
+
+// Função para renderizar os itens do carrinho
+function renderizarItensCarrinho() {
+  const itensCarrinho = document.getElementById("itens-carrinho");
+  itensCarrinho.innerHTML = ""; // Limpa a div antes de renderizar os itens
+
+  let totalPreco = 0; // total de preços
+
+  //cria elementos na div do carrinho apartir do array carrinho
+  carrinho.forEach((item, index) => {
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("item-carrinho");
+
+    const imagemProduto = document.createElement("img");
+    imagemProduto.src = item.imagem;
+    imagemProduto.alt = item.nome;
+    itemDiv.appendChild(imagemProduto);
+
+    const itemNome = document.createElement("span");
+    itemNome.textContent = item.nome;
+
+    const itemPreco = document.createElement("span");
+    itemPreco.textContent = item.preco.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+    totalPreco += item.preco; // soma preço  total
+
+    // button para remover o item do carrinho
+    const removerItemButton = document.createElement("button");
+    removerItemButton.textContent = "Remover";
+    removerItemButton.addEventListener("click", () => {
+      carrinho.splice(index, 1); // remover o item do carrinho
+      renderizarItensCarrinho(); // atualiza a exibição do carrinho
+      atualizarContadorItensCarrinho();
+    });
+
+    itemDiv.appendChild(itemNome);
+    itemDiv.appendChild(itemPreco);
+    itemDiv.appendChild(removerItemButton);
+
+    itensCarrinho.appendChild(itemDiv);
+  });
+
+  // Adicione o total de preço ao final do último elemento
+  const totalDiv = document.createElement("div");
+  totalDiv.classList.add("total-carrinho");
+  totalDiv.textContent = `Total: ${totalPreco.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })}`;
+  itensCarrinho.appendChild(totalDiv);
+}
+
+// Adiciona uma chamada inicial para renderizar os itens do carrinho
+renderizarItensCarrinho();
+
+
+
+/******************************** */
+
+// Função para abrir o modal de adicionar
+function abrirModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "block";
+}
+
+// Função para fechar o modal
+function fecharModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
+
+// "Adicionar" para abrir o modal
+const abrirModalBotao = document.getElementById("abrir-modal");
+abrirModalBotao.addEventListener("click", abrirModal);
+
+//  botão de fechar no modal
+const fecharModalBotao = document.getElementById("fechar-modal");
+fecharModalBotao.addEventListener("click", fecharModal);
+
+// fechar o modal quando clicar fora dele
+window.addEventListener("click", (event) => {
+  const modal = document.getElementById("modal");
+  if (event.target === modal) {
+    fecharModal();
+  }
+});
+
+
+
+
+/***********************/
+
+function adicionarProduto(event) {
+  event.preventDefault(); // Impede o envio padrão do formulário
+
+  // Obtenha os valores dos campos do modal
+  const nome = document.getElementById("produto-nome").value;
+  const descricao = document.getElementById("produto-descricao").value;
+  const tipo = document.getElementById("produto-tipo").value;
+  const preco = parseFloat(document.getElementById("produto-preco").value);
+  const imagem = document.getElementById("produto-imagem").value;
+
+  // Crie um novo objeto representando o produto
+  const novoProduto = {
+    nome,
+    descricao,
+    tipo,
+    preco,
+    imagem,
+  };
+
+  // Adicione o novo produto ao array "produtos"
+  produtos.push(novoProduto);
+
+  // Atualize a exibição dos produtos na página
+  atualizarProdutosNaPagina();
+
+  // Feche o modal
+  fecharModal();
+}
+
+//"Adicionar Produto" no modal
+const botaoAdicionarProduto = document
+  .getElementById("modal")
+  .querySelector("button[type='submit']");
+botaoAdicionarProduto.addEventListener("click", adicionarProduto);
+
+/***************************** */
